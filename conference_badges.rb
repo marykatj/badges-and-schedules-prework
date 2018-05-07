6fd0d32a76c957601ec86c4456aed9b036f98c80
Mary-Kate James <marykatj@gmail.com>
2018-05-07 20:32:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,16 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def badge_
+def batch_badge_creator(names)
+  names.each do |name|
+    puts "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(speakers)
+  counter = 1
+  speakers.each do |name|
+    puts "Hello, #{name}.  You'll be assigned to room #{counter}!"
+    counter += 1
+  end
+end
